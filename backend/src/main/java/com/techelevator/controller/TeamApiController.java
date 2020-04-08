c4637726f0f0d9cdd13f4a29c2e2bb36dd4c9b1f
TaylorB <taylor.broberg@gmail.com>
2020-04-08 13:18:58 -0400

    create team working

--- backend/src/main/java/com/techelevator/controller/TeamApiController.java
+++ backend/src/main/java/com/techelevator/controller/TeamApiController.java
@@ -1,14 +1,25 @@
 package com.techelevator.controller;
 
 import java.util.List;
+
+import javax.validation.Valid;
+
 import com.techelevator.model.JdbcTeamDao;
 import com.techelevator.model.Team;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+<<<<<<< Updated upstream
 import org.springframework.web.bind.annotation.PathVariable;
+=======
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+>>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @CrossOrigin
@@ -28,9 +39,21 @@ public class TeamApiController {
         return teamDao.getAllTeams();
     }
 
+<<<<<<< Updated upstream
     @GetMapping("/user-teams/{id}")
     public List<Team> getAllUsersTeams(@PathVariable long id){
         return teamDao.getTeamsByUser(id);
     }
 
+=======
+    @PostMapping
+    public Team createTeam(@Valid @RequestBody Team team, BindingResult result){
+        
+        if (result.hasErrors()==false) {
+         return teamDao.createTeam(team);
+               
+            }
+        return null;
+    }
+>>>>>>> Stashed changes
 }
