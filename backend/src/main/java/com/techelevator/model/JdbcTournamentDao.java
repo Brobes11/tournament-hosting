fd9d5e04d699a3ed98f51c6083de47ea1bc31a06
alex elsener <alexelsener0@gmail.com>
2020-04-08 16:31:03 -0400

    tournament java classes

---
+++ backend/src/main/java/com/techelevator/model/JdbcTournamentDao.java
@@ -0,0 +1,48 @@
+package com.techelevator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcTournamentDao implements TournamentDao {
+
+    private JdbcTemplate JdbcTemplate;
+
+    @Autowired
+    public JdbcTournamentDao(DataSource dataSource) {
+        this.JdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public List<Tournament> getAllTournaments() {
+        List<Tournament> allTournaments = new ArrayList<>();
+        String sql = "SELECT id, tourney_name, game, start_date, end_date, location, entry_fee, prize_desc FROM "
+                + "tournaments;";
+        SqlRowSet results = JdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            Tournament tournament = mapRowSetTournament(results);
+            allTournaments.add(tournament);
+        }
+        return allTournaments;
+    }
+
+    private Tournament mapRowSetTournament(SqlRowSet results) {
+        Tournament tournament = new Tournament();
+        tournament.setTournamentName(results.getString("tourney_name"));
+        tournament.setGame(results.getString("game"));
+        tournament.setStartDate(results.getDate("start_date").toLocalDate());
+        tournament.setEndDate(results.getDate("end_date").toLocalDate());
+        tournament.setLocation(results.getString("location"));
+        tournament.setEntryFee(results.getInt("entry_fee"));
+        tournament.setPrizeDescription(results.getString("prize_desc"));
+        return tournament;
+    }
+
+}
---
+++ backend/src/main/java/com/techelevator/model/Tournament.java
@@ -0,0 +1,69 @@
+package com.techelevator.model;
+
+import java.time.LocalDate;
+
+public class Tournament {
+    private String tournamentName;
+    private String game;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String location;
+    private int entryFee;
+    private String prizeDescription;
+
+    public String getTournamentName() {
+        return tournamentName;
+    }
+
+    public void setTournamentName(String tournamentName) {
+        this.tournamentName = tournamentName;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getEntryFee() {
+        return entryFee;
+    }
+
+    public void setEntryFee(int entryFee) {
+        this.entryFee = entryFee;
+    }
+
+    public String getPrizeDescription() {
+        return prizeDescription;
+    }
+
+    public void setPrizeDescription(String prizeDescription) {
+        this.prizeDescription = prizeDescription;
+    }
+}
