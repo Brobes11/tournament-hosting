6a9081df92f197180221a576db5fa12794c0620e
Ben <ben.a.p55@gmail.com>
2020-04-08 13:53:26 -0400

    change

--- backend/src/main/java/com/techelevator/controller/TeamApiController.java
+++ backend/src/main/java/com/techelevator/controller/TeamApiController.java
@@ -15,15 +15,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +43,7 @@ public class TeamApiController {
     }
 
     @GetMapping("/team-page")
-    public Team getTeamById(@RequestParam long teamId){
+    public Team getTeamById(@PathVariable long teamId) {
         return teamDao.getTeamById(teamId);
     }
         
@@ -58,7 +52,6 @@ public class TeamApiController {
     public List<Team> getAllUsersTeams(@PathVariable long id){
         return teamDao.getTeamsByUser(id);
     }
-
 
     @PostMapping
     public Team createTeam(@Valid @RequestBody Team team, BindingResult result){
