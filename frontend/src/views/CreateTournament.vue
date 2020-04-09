18cec188f1610475eef0cb2232ae9af776b0244f
alex elsener <alexelsener0@gmail.com>
2020-04-09 12:41:26 -0400

    submitting CreateTournament.vue

---
+++ frontend/src/views/CreateTournament.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-card-title dark large class="elevation-5">Create a New Tournament</v-card-title>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
