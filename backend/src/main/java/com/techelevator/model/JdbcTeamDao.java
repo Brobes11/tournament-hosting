6f9b2e1c56bf475e3fb44133748a2cf6a0e368e3
Cory Swisher <cswishman@gmail.com>
2020-04-13 11:05:54 -0400

    bug fix in join tourn

--- backend/src/main/java/com/techelevator/model/JdbcTeamDao.java
+++ backend/src/main/java/com/techelevator/model/JdbcTeamDao.java
@@ -108,7 +108,7 @@ public class JdbcTeamDao implements TeamDao {
         List<Team> captainsTeams = new ArrayList<>();
 
         String sql = "SELECT teams.id, team_name, game, accepting_members, team_bio from teams "
-                + "JOIN teamroster on teams.id = teamroster.user_id " + "JOIN users on teamroster.user_id = users.id "
+                + "JOIN teamroster on teams.id = teamroster.team_id " + "JOIN users on teamroster.user_id = users.id "
                 + "WHERE captain = true AND teams.game = ? AND teamroster.user_id = ?;";
 
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, game, userId);
