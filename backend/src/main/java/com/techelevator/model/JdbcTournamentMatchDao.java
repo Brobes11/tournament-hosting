49630c3e678f12371a975c9a8e4114914bed11c8
Ben <ben.a.p55@gmail.com>
2020-04-13 18:18:39 -0400

    method name change

--- backend/src/main/java/com/techelevator/model/JdbcTournamentMatchDao.java
+++ backend/src/main/java/com/techelevator/model/JdbcTournamentMatchDao.java
@@ -24,10 +24,10 @@ public class JdbcTournamentMatchDao implements TournamentMatchDao {
         tournamentMatch.setId(results.getLong("match_id"));
         tournamentMatch.setTournamentId(results.getLong("tourney_id"));
         tournamentMatch.setRound(results.getInt("round_number"));
-        tournamentMatch.setTeam1Id(results.getLong("team_1_id"));
-        tournamentMatch.setTeam2Id(results.getLong("team_2_id"));
-        tournamentMatch.setTeam1Score(results.getInt("team_1_score"));
-        tournamentMatch.setTeam2Score(results.getInt("team_2_score"));
+        tournamentMatch.setHomeTeam(results.getLong("team_1_id"));
+        tournamentMatch.setAwayTeam(results.getLong("team_2_id"));
+        tournamentMatch.setHomeScore(results.getInt("team_1_score"));
+        tournamentMatch.setAwayScore(results.getInt("team_2_score"));
         tournamentMatch.setWinnerId(results.getLong("winner_id"));
         return tournamentMatch;
     }
@@ -38,7 +38,7 @@ public class JdbcTournamentMatchDao implements TournamentMatchDao {
             String sql = "INSERT INTO tournamentMatch (tourney_id, round_number, team_1_id, team_2_id) "
                     + "VALUES (?, ?, ?, ?) RETURNING match_id";
             Long newId = jdbcTemplate.queryForObject(sql, Long.class, tournamentMatch.getTournamentId(),
-                    tournamentMatch.getRound(), tournamentMatch.getTeam1Id(), tournamentMatch.getTeam2Id());
+                    tournamentMatch.getRound(), tournamentMatch.getHomeTeam(), tournamentMatch.getAwayTeam());
             tournamentMatch.setId(newId);
         }
         return matches;
@@ -48,7 +48,7 @@ public class JdbcTournamentMatchDao implements TournamentMatchDao {
     public boolean finalizeMatches(List<TournamentMatch> matches) {
         for (TournamentMatch tournamentMatch : matches) {
             String sql = "UPDATE tournamentMatch SET team_1_score = ?, team_2_score = ? winner_id = ? WHERE id = ?;";
-        jdbcTemplate.update(sql, tournamentMatch.getTeam1Score(), tournamentMatch.getTeam2Id(), tournamentMatch.getWinnerId(), tournamentMatch.getId());
+        jdbcTemplate.update(sql, tournamentMatch.getHomeScore(), tournamentMatch.getAwayScore(), tournamentMatch.getWinnerId(), tournamentMatch.getId());
         }
         return true;
     }
