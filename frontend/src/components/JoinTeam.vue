9d1c7a575e93d54115e940c57dc44c4655cc27d8
Cory Swisher <cswishman@gmail.com>
2020-04-15 13:41:34 -0400

    disable joining if already a member

--- frontend/src/components/JoinTeam.vue
+++ frontend/src/components/JoinTeam.vue
@@ -1,106 +1,148 @@
 <template>
   <v-row justify="center">
-
     <v-dialog v-model="dialog" persistent max-width="600px">
       <template v-slot:activator="{ on }">
-        <v-btn color="#03DAC5" dark v-on="on"><v-icon dark>mdi-send</v-icon> Join  </v-btn>
+        <v-btn color="#03DAC5" dark v-on="on" @click="checkIfMember()">
+          <v-icon dark>mdi-send</v-icon>Join
+        </v-btn>
       </template>
       <v-card>
-       
         <v-card-title>
-          <span class="headline">Join Team</span>
+          <v-row>
+            <span v-if="isMember === false" class="headline">Join Team</span>
+            <span v-if="isMember === true" class="headline">You are a member of this team.</span>
+          </v-row>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
               <v-col cols="12">
-                 <v-form ref="joinTeamForm">
-                <v-textarea outlined label="Message Team Captian" v-model="request.message" :rules="msgRules" required></v-textarea>
-              </v-form>
+                <v-form ref="joinTeamForm">
+                  <v-textarea
+                    outlined
+                    label="Message Team Captian"
+                    v-model="request.message"
+                    :rules="msgRules"
+                    :disabled="isMember == true"
+                    required
+                  ></v-textarea>
+                </v-form>
               </v-col>
             </v-row>
           </v-container>
         </v-card-text>
-         
+
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="#03DAC5" text @click="dialog = false;">Close</v-btn>
-          <v-btn color="#03DAC5" text @click=" sendJoinRequest()" @join-success="dialog=false">Join</v-btn>
+          <v-btn
+            color="#03DAC5"
+            text
+            :disabled="isMember == true"
+            @click=" sendJoinRequest()"
+            @join-success="dialog=false"
+          >Join</v-btn>
         </v-card-actions>
-       
       </v-card>
     </v-dialog>
-
   </v-row>
 </template>
 
 <script>
-import auth from '@/auth';
+import auth from "@/auth";
 
-  export default {
-    name:'JoinTeam',
+export default {
+  name: "JoinTeam",
 
-    props:{
-      teamId: Number
+  props: {
+    teamId: Number
+  },
+
+  data: () => ({
+    dialog: false,
+    request: {
+      senderId: "",
+      recipientId: "",
+      message: ""
     },
-      
-    data: () => ({
-      dialog: false,
-      request:{
-        senderId: '',
-        recipientId: '',
-        message: ''
-      },
-      msgRules:[v => !!v || "Please enter a message. ", v => v && v.length >= 10 || " Your message needs to be 10 characters or more."],
+    userTeams: null,
+    isMember: false,
+    msgRules: [
+      v => !!v || "Please enter a message. ",
+      v =>
+        (v && v.length >= 10) ||
+        " Your message needs to be 10 characters or more."
+    ]
+  }),
 
-    }),
-
-      methods:{
-      sendJoinRequest(){
-        if(this.$refs.joinTeamForm.validate()){
-        fetch(`${process.env.VUE_APP_REMOTE_API}/api/team/join-request`,{
-         method:'POST',
-         headers:{
-            Authorization: 'Bearer ' + auth.getToken(),
-            Accept: "application/json",
-           'content-type': 'application/json'
-         },
-         body: JSON.stringify(this.request)
-       })
-       .then(response =>{
-         if(response.ok){
-           this.$emit('join-success');
-           this.dialog = false;
-         }
-         else{
-           this.$emit('duplicate-request');
-           this.dialog=false;
-         }
-       })
+  methods: {
+    sendJoinRequest() {
+      if (this.isMember === false) {
+        if (this.$refs.joinTeamForm.validate()) {
+          fetch(`${process.env.VUE_APP_REMOTE_API}/api/team/join-request`, {
+            method: "POST",
+            headers: {
+              Authorization: "Bearer " + auth.getToken(),
+              Accept: "application/json",
+              "content-type": "application/json"
+            },
+            body: JSON.stringify(this.request)
+          }).then(response => {
+            if (response.ok) {
+              this.$emit("join-success");
+              this.dialog = false;
+            } else {
+              this.$emit("duplicate-request");
+              this.dialog = false;
+            }
+          });
         }
-      },
-      setUser(){
-        this.request.senderId = auth.getUser().id;
-      },
-      setTeam(){
-        this.request.recipientId = this.teamId;
       }
-
     },
-    created(){
-      this.setUser();
-      this.setTeam();
+    setUser() {
+      this.request.senderId = auth.getUser().id;
     },
+    setTeam() {
+      this.request.recipientId = this.teamId;
+    },
+    checkIfMember() {
+      this.userTeams.forEach(team => {
+        if (team.teamId === this.request.recipientId) {
+          this.isMember = true;
+        } else {
+          this.isMember = false;
+        }
+      });
+    }
+  },
+  created() {
+    this.setUser();
+    this.setTeam();
 
-
-
-
-  };
-
+    fetch(
+      `${process.env.VUE_APP_REMOTE_API}/api/team/?userId=${auth.getUser().id}`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Authorization: "Bearer " + auth.getToken()
+        }),
+        credentials: "same-origin"
+      }
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(data => {
+        this.userTeams = data;
+      })
+      .catch(err => console.error(err));
+  }
+};
 </script>
 
 
 
 <style>
-
 </style>
