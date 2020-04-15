6a4ce5b6618a42afeb4c6e810dab3da771febcf8
Ben <ben.a.p55@gmail.com>
2020-04-15 15:44:00 -0400

    more small changes

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -23,9 +23,9 @@
           class="my-3"
           contain
         />
-        <p>We've got some fierce competitors in our selection of awesome teams! check them out to see the best of the best!</p>
-        <v-btn href="/browse-teams">
-           Click here to browse teams
+        <p>We've got some fierce competitors in our selection of awesome teams! Register with us to become the best of the best!</p>
+        <v-btn href="/register">
+           Click here to register
         </v-btn>
       </v-col>
     </v-row>
